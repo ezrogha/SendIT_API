cfbcbdf6d47204038a91f67895a34b817451a011
Roghashin <rtimbiti@gmail.com>
2018-11-10 16:55:07 +0300

    add data file

---
+++ data.py
@@ -0,0 +1,70 @@
+db = {
+    "_response": {
+      "code": "",
+      "message": ""
+    },
+    "parcels": {
+        "1541802758_0": {
+            "from": "Kla",
+            "parcelId": "1541802758_0",
+            "price": "3200",
+            "status": "Not Delivered",
+            "to": "Kigali",
+            "userId": "1541802758",
+            "weight": "1.2"
+        },
+        "1541802758_1": {
+            "from": "Kla",
+            "parcelId": "1541802758_1",
+            "price": "1600",
+            "status": "Not Delivered",
+            "to": "Kisumu",
+            "userId": "1541802758",
+            "weight": "0.7"
+        },
+        "1541802758_2": {
+            "from": "Kla",
+            "parcelId": "1541802758_2",
+            "price": "2200",
+            "status": "Not Delivered",
+            "to": "Da res alaam",
+            "userId": "1541802758",
+            "weight": "0.8"
+        },
+        "1541803030_3": {
+            "from": "Entebbe",
+            "parcelId": "1541803030_3",
+            "price": "40000",
+            "status": "Not Delivered",
+            "to": "New York",
+            "userId": "1541803030",
+            "weight": "0.87"
+        }
+    },
+    "users": {
+        "1541802758": {
+            "address": "Nsambya",
+            "email": "rtimbi@",
+            "firstname": "Rogha",
+            "lastname": "Timbi",
+            "password": "null",
+            "phone": "036489",
+            "received": 0,
+            "sent": 0,
+            "status": "active",
+            "userId": "1541802758"
+        },
+        "1541803030": {
+            "address": "Entebbe",
+            "email": "rorine@",
+            "firstname": "Roshin",
+            "lastname": "Masika",
+            "password": "null",
+            "phone": "043909",
+            "received": 0,
+            "sent": 0,
+            "status": "active",
+            "userId": "1541803030"
+        }
+    }
+}
