97c76c0e392d1434718b7fde8da5e87e1e7db80c
Roghashin <rtimbiti@gmail.com>
2018-11-10 17:26:07 +0300

    Add redirect to /api/v1/

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, redirect, url_for
 from data import db
 
 from datetime import datetime
 from time import mktime
 
 app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return redirect(url_for("index"))
 
 
 @app.route("/api/v1/")
