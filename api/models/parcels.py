5eaf6f09b03cfd7b5f27685aa57a26288e8841de
Roghashin <rtimbiti@gmail.com>
2018-11-14 12:34:16 +0300

    implement fetch parcels endpoints

--- api/models/parcels.py
+++ api/models/parcels.py
@@ -1,8 +1,42 @@
 from api.models.data import db
 
+
 def home_model():
   db["_response"] = {
     "code": 200,
     "message": "Welcome to SendIT api"
   }
   return db["_response"]
+
+
+def get_all_parcels():
+  if(db["parcels"] == {}):
+    db["_response"] = {
+      "code": 200,
+      "message": "No Parcels currently available"
+    }
+    return db["_response"]
+  return db["parcels"]
+
+
+def set_parcel(parcel_id, parcel_userId, parcel_from, parcel_to, parcel_weight, parcel_price, parcel_status):
+  db["parcels"][parcel_id] = {
+      "parcelId": parcel_id,
+      "userId": parcel_userId,
+      "from": parcel_from,
+      "to": parcel_to,
+      "weight": parcel_weight,
+      "price": parcel_price,
+      "status": parcel_status
+  }
+  db["_response"] = {
+      "code": "201",
+      "message": f"A parcel is created by user {parcel_userId}"
+  }
+  parcels = db["parcels"]
+  message = db["_response"]
+  parcels_db = {
+      "_response": message,
+      "parcels": parcels
+  }
+  return parcels_db
--- tests/test_parcel.py
+++ tests/test_parcel.py
@@ -12,16 +12,17 @@ class TestParcels(unittest.TestCase):
   def test_get_parcels(self):
     tester = app.test_client()
     response_post = tester.post(
-        "/api/v1/users/parcels",
+        "/api/v1/parcels",
         data=dict(
+            userId = "",
             p_from="Kampala, Uganda",
             to="khartuom, Sudan",
             weight="1.8"
         )
     )
     response = tester.get("/api/v1/parcels", content_type="html/text")
-    message1 = b"UserId not defined"
-    message2 = b"No Parcels currently available"
-    self.assertIn(message1 in response_post.data)
+    message_post = b"UserId not defined"
+    message = b"No Parcels currently available"
+    self.assertIn(message_post, response_post.data,)
     self.assertEqual(response.status_code, 200)
-    self.assertIn(message2 in response.data)
+    self.assertIn(message, response.data)
