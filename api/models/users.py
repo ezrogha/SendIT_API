80f0bc9d8b200c44bd1f8cdc1696cc8906ecda10
Roghashin <rtimbiti@gmail.com>
2018-12-22 00:57:53 +0300

    Change users.py to OOP

--- api/models/users.py
+++ api/models/users.py
@@ -1,29 +1,37 @@
 from api.models.database import DBConnection
 
-DBConn = DBConnection()
 
-def setUser(username, email, phone, address, password, role):
-    result = DBConn.add_user(username, email, phone, address, password, role)
-    if result == "Already exists":
-        return {"message": "username or email already used"}
-    return {"message": "Account has been created"}
+class User(object):
+
+    def __init__(self):
+        self.DBConn = DBConnection()
 
 
-def loginUser(username, password):
-    user = DBConn.login_user(username, password)
-    if user == None:
-        return {"message": "User doesnot exist"}
-    return user
+    def setUser(self, username, email, phone, address, password, role):
+        result = self.DBConn.add_user(username, email, phone, address, password, role)
+        if result == "Already exists":
+            return {"message": "username or email already used"}
+        return {"message": "Account has been created"}
 
-def deleteTables():
-    DBConn.delete_tables()
 
-def specificUserparcels(userId):
-    result = DBConn.view_user_parcels(userId)
-    if result == None:
-        return {"message": "User doesnot exist"}
-    return result
+    def loginUser(self, username, password):
+        user = self.DBConn.login_user(username, password)
+        if user == None:
+            return {"message": "User doesnot exist"}
+        return user
 
-def fetchAllUsers():
-    result = DBConn.all_users()
-    return result
+
+    def deleteTables(self):
+        self.DBConn.delete_tables()
+
+
+    def specificUserparcels(self, userId):
+        result = self.DBConn.view_user_parcels(userId)
+        if result == None:
+            return {"message": "User doesnot exist"}
+        return result
+
+
+    def fetchAllUsers(self):
+        result = self.DBConn.all_users()
+        return result
